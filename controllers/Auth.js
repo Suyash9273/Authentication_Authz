3dac2de529b5da20b299e344d97f50e34b12f3ee
Suyash <suyashrajsingh424@gmail.com>
2025-07-12 19:55:50 +0530

    4rth commit

---
+++ controllers/Auth.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+const User = require("../models/User");
+
+//signup route handler: -->
+exports.signup = async (req, res) => {
+    try {
+        //get data:
+        const {name, email, password, role} = req.body;
+        //check if user already exists??
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: "User already exists"
+                }
+            );
+        }
+        //secure password: 
+        let hashedPassword;
+        try{
+            hashedPassword = bcrypt.hash(password, 10);
+        }
+        catch(err){
+            return res.status(500).json({
+                success: false,
+                message: "Error in encrypting password"
+            })
+        }
+
+        //create entry for user:
+        const user = await User.create({
+            name, email, password: hashedPassword, role
+        })
+
+        res.status(200).json({
+            success: true,
+            message: "User created successfully"
+        })
+
+    } catch (error) {
+        
+    }
+}
--- index.js
+++ index.js
@@ -5,3 +5,13 @@ require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+
+const connectDB = require("./config/db");
+connectDB();
+const route = require("./routes/route");
+app.use("/api/v1", route);
+
+//activate: 
+app.listen(PORT, () => {
+    console.log(`App is listening at port: ${PORT}`);
+})
